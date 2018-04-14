99e98bb39cd280591156a95fe5bddae079a5bd9a
Ruba Aburub <abualrubruba@gmail.com>
2018-04-15 02:13:37 +0300

    This commit was done for the GET deals route in order to return all deals searched by the user depending on some filtering params and validates the results if it contains empty data

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
+var url = require('url');
+
+
+app.get('/deals', (req, res) => {
+    var url_parts = url.parse(req.url, true)
+    var destination = url_parts.query.destination
+    var lengthOfStay = url_parts.query.lengthOfStay
+    var minTripStartDate = url_parts.query.minTripStartDate
+    var maxTripStartDate = url_parts.query.maxTripStartDate
+    var maxStarRating = url_parts.query.maxStarRating
+    var minStarRating = url_parts.query.minStarRating
+    var minTotalRate = url_parts.query.minTotalRate
+    var maxTotalRate = url_parts.query.maxTotalRate
+    expediaUrl = `https://offersvc.expedia.com/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=foo&productType=Hotel`
+    if (destination) expediaUrl = expediaUrl + `&destinationName=${destination}`
+    if (lengthOfStay) expediaUrl = expediaUrl + `&lengthOfStay=${lengthOfStay}`
+    if (minTripStartDate) expediaUrl = expediaUrl + `&minTripStartDate=${minTripStartDate}`
+    if (maxTripStartDate) expediaUrl = expediaUrl + `&maxTripStartDate=${maxTripStartDate}`
+    if (maxStarRating) expediaUrl = expediaUrl + `&maxStarRating=${maxStarRating}`
+    if (minStarRating) expediaUrl = expediaUrl + `&minStarRating=${minStarRating}`
+    if (minTotalRate) expediaUrl = expediaUrl + `&minTotalRate=${minTotalRate}`
+    if (maxTotalRate) expediaUrl = expediaUrl + `&maxTotalRate=${maxTotalRate}`
+
+    axios.get(expediaUrl)
+        .then(function (response) {
+            //Check if parameters contain empty data 
+            if (Object.keys(response.data.offers).length === 0) {
+
+                res.send('No Data Found')
+            }
+            //this will retrieve data from applied parameters
+            res.send(
+
+                response.data.offers.Hotel.map(deal => {
+                    return {
+                        "hotelId": deal.hotelInfo.hotelId,
+                        "hotelName": deal.hotelInfo.hotelName,
+                        "hotelCity": deal.hotelInfo.hotelCity,
+                        "hotelLat": deal.hotelInfo.hotelLatitude,
+                        "hotelLng": deal.hotelInfo.hotelLongitude,
+                        "hotelStarRating": deal.hotelInfo.hotelStarRating,
+                        "totalReviews": deal.hotelInfo.hotelReviewTotal,
+                        "hotelImageUrl": deal.hotelInfo.hotelImageUrl,
+                        "vipAccess": deal.hotelInfo.vipAccess,
+                        "totalPrice": deal.hotelPricingInfo.totalPriceValue,
+                        "originalPricePerNight": deal.hotelPricingInfo.originalPricePerNight,
+                        "priceCurrency": deal.hotelPricingInfo.currency,
+                        "percentageSaving": deal.hotelPricingInfo.percentSavings
+                    }
+                })
+            )
+
+
+        })
+
+        .catch(function (error) {
+            console.log(error);
+        });
+})
+
+app.listen(4000, () => console.log('Example app listening on port 4000!'))
