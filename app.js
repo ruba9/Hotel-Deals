7da705d9278172a4c17ac38bf2e983f2e4341bde
Ruba Aburub <abualrubruba@gmail.com>
2018-04-15 16:11:09 +0300

    change port to process.env

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-  
+
 app.get('/deals', (req, res) => {
     var url_parts = url.parse(req.url, true)
     var destination = url_parts.query.destination
@@ -66,4 +66,4 @@ app.get('/deals', (req, res) => {
         });
 })
 
-app.listen(4000, () => console.log('Example app listening on port 4000!'))
+app.listen(process.env.PORT, () => console.log('Example app listening on port 4000!'))
